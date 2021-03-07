6bf8e20cf7e97a072ab00fc9e1fcd0dc775fe1cf
lightbulblighter <59720715+lightbulblighter@users.noreply.github.com>
2021-03-06 21:03:12 -0600

    [minor]: Rboxlo.Core.Util::FormatBytes

--- Client/Rboxlo.Core/Util.cs
+++ Client/Rboxlo.Core/Util.cs
@@ -19,5 +19,26 @@ namespace Rboxlo.Core
         {
             return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(input.ToLower());
         }
+
+        /// <summary>
+        /// Formats bytes to human readable form
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        /// <param name="decimals">Max amount of decimals to display</param>
+        /// <param name="bi">Bisexual form</param>
+        public static string FormatBytes(int bytes, int decimals = 2, bool bi = true)
+        {
+            if (bytes == 0)
+            {
+                return "";
+            }
+
+            int k = bi ? 1024 : 1000;
+            int dm = decimals < 0 ? 0 : decimals;
+            string[] sizes = bi ? new string[] { "Bytes", "KiB", "MiB", "GiB", "TiB", "PiB" } : new string[] { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+
+            int idx = Convert.ToInt32(Math.Floor(Math.Log(bytes) / Math.Log(k)));
+            return String.Format("{0} {1}", ((bytes / Math.Pow(k, idx)).ToString(String.Format("N{0}", dm))), sizes[idx]);
+        }
     }
 }
