f88997bcd31a2c18c24b5f824a6df90f583e7354
lightbulblighter <59720715+lightbulblighter@users.noreply.github.com>
2021-03-13 23:21:05 -0600

    [feat]: strcmp, strcicmp handlebars helpers

--- Server/hbh.js
+++ Server/hbh.js
@@ -1,5 +1,15 @@
+// handlebars turing complete when??
+
 var exports = module.exports = {}
 
 const util = require("./util")
 
 exports.titlecase = util.titlecase
+
+exports.strcmp = (x, y, opt) => {
+    return (x == y) ? opt.fn(this) : opt.inverse(this)
+}
+
+exports.strcicmp = (x, y, opt) => {
+    return (x.toLowerCase() == y.toLowerCase()) ? opt.fn(this) : opt.inverse(this)
+}
